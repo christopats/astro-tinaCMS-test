3bed2e3c5da5084572c1436a1f6f20438a459fd7
christopats <christopattison@gmail.com>
2024-12-17 15:43:16 +0000

    removed teraform and tina to connect with firebase

--- terraform/build/buckets.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "raw" {
-  project = var.gcp_project_id
-  name = "${var.gcp_data_project}-raw"
-  force_destroy = false
-  uniform_bucket_level_access = true
-  location = var.gcp_region
-  labels = local.labels
-}
